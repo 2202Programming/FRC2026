8914a5ceb929739fef6cb0950e1a6c1aacb60b01
Beast2026C <dev_team@beastrobotics.com>
2026-01-21 20:03:16 -0600

    Adding BOB Epsilon
    Adding BOB Epsilon

--- src/main/java/frc/Main.java
+++ src/main/java/frc/Main.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.RobotBase;
 import frc.lib2202.builder.Robot;
 import frc.robot2026.RobotSpec_Alpha2026;
 import frc.robot2026.RobotSpec_BotOnBoard1;
+import frc.robot2026.RobotSpec_BotOnBoard_Epsilon;
 
 public final class Main {
   private Main() {
@@ -17,6 +18,7 @@ public final class Main {
     // 2026 sub-tree
     new RobotSpec_Alpha2026(); 
     new RobotSpec_BotOnBoard1();
+    new RobotSpec_BotOnBoard_Epsilon();
 
   }
   public static void main(String... args) {
