c0f3ac40ac59eea96b05aae8d2d6296634bfde3c
DerekL <derek@adminnet.us>
2026-02-10 14:16:51 -0600

    added spec for Finn

---
+++ src/main/java/frc/robot2026/RobotSpec_ChassisBot_Finn.java
@@ -0,0 +1,199 @@
+package frc.robot2026;
+
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.FeetPerSecond;
+import static frc.lib2202.Constants.MperFT;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.commands.PathfindingCommand;
+import com.revrobotics.spark.SparkFlex;
+
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.lib2202.builder.IRobotSpec;
+import frc.lib2202.builder.RobotContainer;
+import frc.lib2202.builder.RobotLimits;
+import frc.lib2202.builder.SubsystemConfig;
+import frc.lib2202.command.swerve.FieldCentricDrive;
+import frc.lib2202.subsystem.OdometryInterface;
+import frc.lib2202.subsystem.Sensors;
+import frc.lib2202.subsystem.hid.HID_Subsystem;
+import frc.lib2202.subsystem.swerve.AutoPPConfigure;
+import frc.lib2202.subsystem.swerve.DriveTrainInterface;
+import frc.lib2202.subsystem.swerve.IHeadingProvider;
+import frc.lib2202.subsystem.swerve.SwerveDrivetrain;
+import frc.lib2202.subsystem.swerve.config.ChassisConfig;
+import frc.lib2202.subsystem.swerve.config.ModuleConfig;
+import frc.lib2202.subsystem.swerve.config.ModuleConfig.CornerID;
+import frc.lib2202.util.PIDFController;
+import frc.robot2026.Constants.CAN;
+import frc.robot2026.subsystems.Climber;
+import frc.robot2026.subsystems.VisionPoseEstimator;
+import frc.robot2026.testBindings.DpltestBinding;
+
+public class RobotSpec_ChassisBot_Finn implements IRobotSpec { 
+  // $env:serialnum = "03282B65"
+
+  final SubsystemConfig ssconfig = new SubsystemConfig("ChassisBot_FIN", "FINN_FIX_ME")
+      // deferred construction via Supplier<Object> lambda
+      .add(PowerDistribution.class, "PDP", () -> {
+        var pdp = new PowerDistribution(CAN.PDP, ModuleType.kRev);
+        pdp.clearStickyFaults();
+        return pdp;
+      })
+      .add(HID_Subsystem.class, "DC", () -> {
+        return new HID_Subsystem(0.3, 0.9, 0.05);
+      })
+      .add(Sensors.class, "sensors", () -> {
+        return new Sensors(CAN.PIGEON_IMU_CAN);
+      })
+     
+      .add(SwerveDrivetrain.class, "drivetrain", () -> {
+        return new SwerveDrivetrain(SparkFlex.class);
+      })
+     ;
+
+
+
+  // Robot Speed Limits
+  RobotLimits robotLimits = new RobotLimits(FeetPerSecond.of(15.0), DegreesPerSecond.of(180.0));
+
+  // Chassis
+  double kWheelCorrectionFactor = .9875;
+  double kSteeringGR = 21.428;
+  double kDriveGR = 6.12;
+  double kWheelDiameter = MperFT * 4.0 / 12.0; // [m]
+
+  final ChassisConfig chassisConfig = new ChassisConfig(
+      0.58 / 2.0,  //coord front-left x
+      0.58 / 2.0,  //coord front-left y
+      kWheelCorrectionFactor, // scale [] <= 1.0
+      kWheelDiameter,
+      kSteeringGR,
+      kDriveGR,
+      //fix kf for rev voltage ctrl and not pct-pwr
+      new PIDFController(0.085, 0.00055, 0.0, 12.0*0.21292), // drive
+      new PIDFController(0.01, 0.0, 0.0, 0.0) // angle
+  );
+
+  public RobotSpec_ChassisBot_Finn() {
+    // finish BetaBot's drivePIDF
+    chassisConfig.drivePIDF.setIZone(0.2);
+    // add the specs to the ssconfig
+    ssconfig.setRobotSpec(this);
+  }
+
+  // Required method that use the specs above
+
+  @Override
+  public RobotLimits getRobotLimits() {
+    return robotLimits;
+  }
+
+  @Override
+  public IHeadingProvider getHeadingProvider() {
+    return RobotContainer.getSubsystem("sensors");
+  }
+
+  @Override
+  public ChassisConfig getChassisConfig() {
+    return chassisConfig;
+  }
+
+  @Override
+  public ModuleConfig[] getModuleConfigs() {
+    // TODO - correct offsets for FINN - maybe grab from 2025
+    ModuleConfig[] modules = new ModuleConfig[4];
+    modules[CornerID.FrontLeft.getIdx()] = new ModuleConfig(CornerID.FrontLeft,
+        CAN.FL_CANCoder, CAN.FL_Drive, CAN.FL_Angle, 41.17587)
+        .setInversions(false, true, false);
+
+    modules[CornerID.FrontRight.getIdx()] = new ModuleConfig(CornerID.FrontRight,
+        CAN.FR_CANCoder, CAN.FR_Drive, CAN.FR_Angle, -63.98)
+        .setInversions(true, true, false);
+
+    modules[CornerID.BackLeft.getIdx()] = new ModuleConfig(CornerID.BackLeft,
+        CAN.BL_CANCoder, CAN.BL_Drive, CAN.BL_Angle, 50.45)
+        .setInversions(false, true, false);
+
+    modules[CornerID.BackRight.getIdx()] = new ModuleConfig(CornerID.BackRight,
+        CAN.BR_CANCoder, CAN.BR_Drive, CAN.BR_Angle, -66.27)
+        .setInversions(true, true, false);
+
+    return modules;
+  }
+
+  @Override
+  public void setBindings() {
+    // String odometryName = VisionPoseEstimator.class.getSimpleName(); // or
+    // novision "odometry"
+    // TODO switch to vision based when we have a LL
+    OdometryInterface odo = RobotContainer.getSubsystemOrNull("odometry");
+    VisionPoseEstimator vpe = RobotContainer.getSubsystemOrNull("vision_odo");
+    DriveTrainInterface sdt = RobotContainer.getSubsystemOrNull("drivetrain");
+    HID_Subsystem dc = RobotContainer.getSubsystem("DC");
+    Climber cl = RobotContainer.getSubsystem("climber");
+
+    CommandXboxController operator = (CommandXboxController)dc.Operator();
+
+    // Initialize PathPlanner, if we have needed Subsystems
+    if (odo != null && sdt != null) {
+      AutoPPConfigure.configureAutoBuilder(sdt, odo);
+      var cmd = PathfindingCommand.warmupCommand();
+      CommandScheduler.getInstance().schedule(cmd);
+    }
+
+    // Competition bindings
+    BindingsCompetition.ConfigureCompetition(dc, false);
+    
+    //Take care testing binding don't collide
+    DpltestBinding.calbrate(operator);  // rt/left bumper, rt/lt Trigger
+    cl.setDemoBindings(operator);       //pov,a,x
+
+    // Place your test binding in ./testBinding/<yourFile>.java and call it here
+    // comment out any conflicting bindings. Try not to push with your bindings
+    // active. Just comment them out.
+
+    // Anything else that needs to run after binding/commands are created
+    
+    // maybe beter way, but this registers vpe with the aliance-aware reset cmd.
+    if (vpe != null) vpe.configureGyroCallback();
+
+    // show what cmds are running
+    SmartDashboard.putData(CommandScheduler.getInstance());
+  }
+
+  SendableChooser<Command> autoChooser;
+
+  @Override
+  public void setupRegisteredCommands() {
+    RegisteredCommands.RegisterCommands();
+
+    // enable chooser - builds autochooser list, requires AutoBuilder to be
+    // configured
+    // thus SDT and some form of odometry. Skip auto if not configured.
+    if (AutoBuilder.isConfigured()) {
+      autoChooser = AutoBuilder.buildAutoChooser();
+      SmartDashboard.putData("Auto Chooser", autoChooser);
+    }
+  }
+
+  @Override
+  public SendableChooser<Command> getChooser() {
+    return autoChooser;
+  }
+
+  @Override
+  public void setDefaultCommands() {
+    DriveTrainInterface drivetrain = RobotContainer.getSubsystemOrNull("drivetrain");
+    if (drivetrain != null) {
+      drivetrain.setDefaultCommand(new FieldCentricDrive());
+    }
+  }
+
+}
