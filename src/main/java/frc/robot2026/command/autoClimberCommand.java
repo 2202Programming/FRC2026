9bbce3d5918e6f3315339979b6bbef16cd56dc09
Gavin <gavin.r.longley@gmail.com>
2026-02-21 09:50:24 -0600

    Mistakes were made, I apologize

--- src/main/java/frc/robot2026/command/autoClimberCommand.java
+++ src/main/java/frc/robot2026/command/autoClimberCommand.java
@@ -4,6 +4,7 @@
 
 package frc.robot2026.command;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.lib2202.builder.RobotContainer;
 import frc.lib2202.subsystem.swerve.SwerveDrivetrain;
@@ -17,21 +18,21 @@ public class autoClimberCommand extends SequentialCommandGroup {
   /** Creates a new autoClimberCommand. */
   Climber climber;
   SwerveDrivetrain dt;
-  double distance;
 
-  public autoClimberCommand(Boolean leftSide) {
+
+  public autoClimberCommand(boolean leftSide) {
     climber = RobotContainer.getSubsystem(Climber.class);
     dt = RobotContainer.getSubsystem(SwerveDrivetrain.class);
-    if (leftSide) {
-      distance = -0.5; //meters
-    } else {
-      distance = 0.5;
-    }
     
 
     addRequirements(climber, dt);
     // Use addRequirements() here to declare subsystem dependencies
-    addCommands(climber.armsToPoint(climber.climbposition()), new goDistance(distance), climber.armsToPoint(0));
+    if (leftSide) {
+      addCommands(climber.armsToPoint(climber.climbposition()), new goDistance(0), climber.armsToPoint(0));
+    } else {
+      addCommands(climber.armsToPoint(climber.climbposition()), new goDistance(0), climber.armsToPoint(0));
+    }
+    
     // order of opp: send arms to a position (predetermined by the subsystem), dive backwards X amount, then arms to position 0.)
     //The command itself is not difficult, the issues come other forms.
 
