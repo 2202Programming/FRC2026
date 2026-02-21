a506af824ff1d6831a9225b93061254b43ebfb32
Gavin <gavin.r.longley@gmail.com>
2026-02-21 14:41:56 -0600

    Finished what ben started, need to add Interpolation table

--- src/main/java/frc/robot2026/subsystems/Shooter/Targeter.java
+++ src/main/java/frc/robot2026/subsystems/Shooter/Targeter.java
@@ -1,5 +1,8 @@
 package frc.robot2026.subsystems.Shooter;
 
+import java.util.Optional;
+
+import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
 import edu.wpi.first.math.interpolation.Interpolator;
@@ -11,6 +14,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib2202.builder.RobotContainer;
 import frc.lib2202.subsystem.OdometryInterface;
+import frc.robot2026.Constants.TheField;
 
 /*
 manage shooter speeds for different command use
@@ -28,11 +32,24 @@ public class Targeter extends SubsystemBase {
 
     final double HIGH_SPEED = 55.0;
     final double LOW_SPEED = 25.0;
+    final double Shooter_Angle = 70.0; //Degrees
+
+    Translation2d blueTarget;
+    Translation2d redTarget;
 
     double distToTarget; // function of VPE pose and Hub center + math
     double manual_speed; // flywheel speed manually controlled by driver
 
     public Targeter() {
+
+        Pose3d blue1 = TheField.fieldLayout.getTagPose(20).get();
+        Pose3d blue2 = TheField.fieldLayout.getTagPose(26).get();
+        blueTarget = new Translation2d(Math.abs(blue1.getX()-blue2.getX()) / 2.0, blue1.getY());
+
+        Pose3d red1 = TheField.fieldLayout.getTagPose(4).get();
+        Pose3d red2 = TheField.fieldLayout.getTagPose(10).get();
+        redTarget = new Translation2d(Math.abs(red1.getX()-red2.getX()) / 2.0, red1.getY()); 
+
         odo = RobotContainer.getSubsystem("odometry");
 
         InverseInterpolator<Double> distance = InverseInterpolator.forDouble();
@@ -40,7 +57,7 @@ public class Targeter extends SubsystemBase {
 
         rpm_table = new InterpolatingTreeMap<>(distance, rpm);
 
-        rpm_table.put(null, null);
+        rpm_table.put(null, null); //TODO These values will need to be tested for.
         rpm_table.put(null, null);
         rpm_table.put(null, null);
         rpm_table.put(null, null);
@@ -67,6 +84,13 @@ public class Targeter extends SubsystemBase {
     public void setTarget() {
         var optAlliance = DriverStation.getAlliance(); // make sure this is accurate :)
         var alliance = optAlliance.isPresent() ? optAlliance.get() : DriverStation.Alliance.Blue;
+        targetTranslation2d = (alliance == DriverStation.Alliance.Blue ? 
+            blueTarget:redTarget);
+
+        if (!optAlliance.isPresent())
+            System.out.println("Warning: As no alliance was recived, we are defaulting to the Blue Alliance hub. This will go badly");
+        // call calculate() to ensure update if target changed
+        calculate();
     }
 
     // Basic Commands
