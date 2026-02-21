de8b236e6c1cf2809be1d5b0f4bda1c10e45bfd9
Beast Robotics <dev_team@beastrobotics.com>
2026-02-21 13:53:50 -0600

    started distance interpol
    -- BG (for GL)

--- src/main/java/frc/robot2026/subsystems/Shooter/Targeter.java
+++ src/main/java/frc/robot2026/subsystems/Shooter/Targeter.java
@@ -1,9 +1,16 @@
 package frc.robot2026.subsystems.Shooter;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
+import edu.wpi.first.math.interpolation.Interpolator;
+import edu.wpi.first.math.interpolation.InverseInterpolator;
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib2202.builder.RobotContainer;
+import frc.lib2202.subsystem.OdometryInterface;
 
 /*
 manage shooter speeds for different command use
@@ -13,6 +20,12 @@ manage shooter speeds for different command use
     heading to  hub 
  */
 public class Targeter extends SubsystemBase {
+    final OdometryInterface odo;
+    Translation2d targetTranslation2d;
+
+    InterpolatingTreeMap<Double, Double> rpm_table; // [M/S]
+    double meas_dist;
+
     final double HIGH_SPEED = 55.0;
     final double LOW_SPEED = 25.0;
 
@@ -20,15 +33,42 @@ public class Targeter extends SubsystemBase {
     double manual_speed; // flywheel speed manually controlled by driver
 
     public Targeter() {
+        odo = RobotContainer.getSubsystem("odometry");
 
+        InverseInterpolator<Double> distance = InverseInterpolator.forDouble();
+        Interpolator<Double> rpm = Interpolator.forDouble();
+
+        rpm_table = new InterpolatingTreeMap<>(distance, rpm);
+
+        rpm_table.put(null, null);
+        rpm_table.put(null, null);
+        rpm_table.put(null, null);
+        rpm_table.put(null, null);
+        rpm_table.put(null, null);
     }
 
-    @Override
-    public void initSendable(SendableBuilder builder) {
-
+    // === INTERPOL GETTERS / SETTERS ===
+    public double getRPMFromDistance(double distance) {
+        return rpm_table.get(distance);
     }
 
-    // cmd factory for use in complex commands
+    public void calculate() {
+        meas_dist = odo.getDistanceToTranslation(targetTranslation2d);
+    }
+
+    public double getTargetRPM() {
+        return getRPMFromDistance(meas_dist);
+    }
+
+    public double getTargetDistance() {
+        return meas_dist;
+    }
+    
+    public void setTarget() {
+        var optAlliance = DriverStation.getAlliance(); // make sure this is accurate :)
+        var alliance = optAlliance.isPresent() ? optAlliance.get() : DriverStation.Alliance.Blue;
+    }
+
     // Basic Commands
     public Command manualHigh() {
         return runOnce(() -> {
@@ -41,6 +81,10 @@ public class Targeter extends SubsystemBase {
         return runOnce(() -> {
             manual_speed = LOW_SPEED;
         });
+    }
+
+    @Override
+    public void initSendable(SendableBuilder builder) {
 
     }
 }
