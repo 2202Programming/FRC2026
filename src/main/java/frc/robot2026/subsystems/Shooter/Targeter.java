6edee860ef28ae97292dd5f51d85e3ff64b16252
DerekL <derek@adminnet.us>
2026-02-21 22:04:27 -0600

    Targeter table entered, zero vel fix.

--- src/main/java/frc/robot2026/subsystems/Shooter/Targeter.java
+++ src/main/java/frc/robot2026/subsystems/Shooter/Targeter.java
@@ -1,5 +1,7 @@
 package frc.robot2026.subsystems.Shooter;
 
+import static frc.lib2202.Constants.MperFT;
+
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.interpolation.InterpolatingTreeMap;
@@ -24,61 +26,67 @@ public class Targeter extends SubsystemBase {
     final OdometryInterface odo;
     Translation2d targetTranslation2d;
 
-    InterpolatingTreeMap<Double, Double> rpm_table; // [M/S]
+    InterpolatingTreeMap<Double, Double> vel_table; // [M/S]
     double meas_dist;
 
     final double HIGH_SPEED = 26.8; // [M/S]
     final double LOW_SPEED = 26.8; // [M/S]
     
     // Provided by vince as angle between the center of the motor and the trailing edge of the ball exit ramp
-    final double Shooter_Angle = 65.0; //Degrees
+    final double Shooter_Angle = 65.0; // [deg]
 
-    Translation2d blueTarget;
-    Translation2d redTarget;
+    //Hub 
+    final Translation2d blueHubTarget;
+    final Translation2d redHubTarget;
 
     double distToTarget; // function of VPE pose and Hub center + math
-    double manual_speed; // flywheel speed manually controlled by driver
+    double manual_speed = LOW_SPEED; // flywheel speed manually controlled by driver
 
-    
     public Targeter() {
-
+        this("vision_odo");
+    }
+    
+    public Targeter(String odo_name) {
+        //use hub tags to calc center of blue and red hubs
         Pose3d blue1 = TheField.fieldLayout.getTagPose(20).get();
         Pose3d blue2 = TheField.fieldLayout.getTagPose(26).get();
-        blueTarget = new Translation2d(Math.abs(blue1.getX()+blue2.getX()) / 2.0, blue1.getY());
+        blueHubTarget = new Translation2d((blue1.getX()+blue2.getX()) / 2.0, blue1.getY());
 
         Pose3d red1 = TheField.fieldLayout.getTagPose(4).get();
         Pose3d red2 = TheField.fieldLayout.getTagPose(10).get();
-        redTarget = new Translation2d(Math.abs(red1.getX()+red2.getX()) / 2.0, red1.getY()); 
+        redHubTarget = new Translation2d((red1.getX()+red2.getX()) / 2.0, red1.getY()); 
 
-        odo = RobotContainer.getSubsystem("odometry");
+        odo = RobotContainer.getSubsystem(odo_name);
 
         InverseInterpolator<Double> distance = InverseInterpolator.forDouble();
-        Interpolator<Double> rpm = Interpolator.forDouble();
+        Interpolator<Double> vel_mps = Interpolator.forDouble();
 
-        rpm_table = new InterpolatingTreeMap<>(distance, rpm);
-        // https://docs.google.com/spreadsheets/d/1Cv1TSaGrY6Wx_dROy699KGdKHrFOiZqe/
-        // physics based pre-calculations:
-        rpm_table.put(0.5, 30.0); //TODO These values will need to be tested for.
-        rpm_table.put(1.0, 35.0);
-        rpm_table.put(3.0, 50.0);
-        rpm_table.put(4.5, 75.0);
-        rpm_table.put(6.0, 100.0);
+        vel_table = new InterpolatingTreeMap<>(distance, vel_mps);
+        
+        // Quick and dirty table measured on 2/21/26  
+        // distance[m] -> flywheel [m/s]
+        vel_table.put(0.0 * MperFT, 22.0);      // set a min
+        vel_table.put(5.0 * MperFT, 22.0); 
+        vel_table.put(6.0 * MperFT, 22.7);
+        vel_table.put(10.0 * MperFT, 26.8);     // this is ladder radius
+        vel_table.put(17.0 * MperFT, 32.5);
+        vel_table.put(25.0 * MperFT, 32.5);     //set a max
     }
     public double getManualSpeed(){
         return manual_speed;
     }
 
     // === INTERPOL GETTERS / SETTERS ===
-    public double getRPMFromDistance(double distance) {
-        return rpm_table.get(distance);
+    public double getVelFromDistance(double distance) {
+        return vel_table.get(distance);
     }
 
     public void calculate() {
         meas_dist = odo.getDistanceToTranslation(targetTranslation2d);
     }
 
-    public double getTargetRPM() {
-        return getRPMFromDistance(meas_dist);
+    public double getTargetVelocity() {
+        return getVelFromDistance(meas_dist);
     }
 
     public double getTargetDistance() {
@@ -89,7 +97,7 @@ public class Targeter extends SubsystemBase {
         var optAlliance = DriverStation.getAlliance(); // make sure this is accurate :)
         var alliance = optAlliance.isPresent() ? optAlliance.get() : DriverStation.Alliance.Blue;
         targetTranslation2d = (alliance == DriverStation.Alliance.Blue ? 
-            blueTarget:redTarget);
+            blueHubTarget : redHubTarget);
 
         if (!optAlliance.isPresent())
             System.out.println("Warning: As no alliance was recived, we are defaulting to the Blue Alliance hub. This will go badly");
