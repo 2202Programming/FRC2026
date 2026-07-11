373431f731bf11a5a7503ff21f39c58d4980b679
Beast2026C <dev_team@beastrobotics.com>
2026-07-11 09:44:14 -0500

    updated encoder numbers

--- src/main/java/frc/robot2026/RobotSpec_AlphaBot.java
+++ src/main/java/frc/robot2026/RobotSpec_AlphaBot.java
@@ -200,20 +200,20 @@ public class RobotSpec_AlphaBot implements IRobotSpec {
   public ModuleConfig[] getModuleConfigs() {
     ModuleConfig[] modules = new ModuleConfig[4];
     modules[CornerID.FrontLeft.getIdx()] = new ModuleConfig(CornerID.FrontLeft,
-        CAN.FL_CANCoder, CAN.FL_Drive, CAN.FL_Angle, -19.42)
+        CAN.FL_CANCoder, CAN.FL_Drive, CAN.FL_Angle, -17.838)
         .setInversions(false, true, true);
 
     modules[CornerID.FrontRight.getIdx()] = new ModuleConfig(CornerID.FrontRight,
-        CAN.FR_CANCoder, CAN.FR_Drive, CAN.FR_Angle, -103.54)
+        CAN.FR_CANCoder, CAN.FR_Drive, CAN.FR_Angle, -105.737)
         .setInversions(true, true, true);
         //.withWheelFactor(3.98/3.93);   shouldn't need this anymore - changed wheels out// replaced wheel / other wheels dia, applies only to FR
 
     modules[CornerID.BackLeft.getIdx()] = new ModuleConfig(CornerID.BackLeft,
-        CAN.BL_CANCoder, CAN.BL_Drive, CAN.BL_Angle, -126.48)
+        CAN.BL_CANCoder, CAN.BL_Drive, CAN.BL_Angle, -142.124)
         .setInversions(false, true, true);
 
     modules[CornerID.BackRight.getIdx()] = new ModuleConfig(CornerID.BackRight,
-        CAN.BR_CANCoder, CAN.BR_Drive, CAN.BR_Angle, 102.64)
+        CAN.BR_CANCoder, CAN.BR_Drive, CAN.BR_Angle, 102.024)
         .setInversions(true, true, true);
 
     return modules;
